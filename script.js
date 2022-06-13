f54e44c991125933e19b8dd43d65db37092c110a
njacques47 <nojacques94@gmail.com>
2022-06-13 02:16:03 -0400

    winging local storage

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+$(document).ready()
 // update tasks by the hour
 
 // check current time
 
 // add to local storage
+$(".save-btn").on("click", function () {
+  var value = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
+  localStorage.setItem(time, value);
+});
 
 // display the day
+
